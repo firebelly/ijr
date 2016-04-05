6fc9f1c3f1d5d40a9d60164a528fd305900c9c0d
Bryant Smith <bryant.robert.smith@gmail.com>
2016-04-05 14:27:51 -0500

    make logo link to home page;

--- web/app/themes/thecenter/templates/header.php
+++ web/app/themes/thecenter/templates/header.php
@@ -4,7 +4,7 @@
       <h1>the <span class="-center">Center</span></h1>
     </a>
   </hgroup>
-  <svg role="image" class="icon icon-ijr"><use xlink:href="#icon-ijr"></use></svg>
+  <a href="<?= esc_url(home_url('/')); ?>"><svg role="image" class="icon icon-ijr"><use xlink:href="#icon-ijr"></use></svg></a>
   <div class="nav-toggle outside">
     <h2 class="menu">Menu</h2>
     <svg role="image" class="icon icon-burger nav-icon"><use xlink:href="#icon-burger"></use></svg>
